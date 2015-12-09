d000a5090de2e146ccabf82e6abf81b5b0a1de77
Thanh-Phong <hotv@clarkson.edu>
2015-12-08 22:16:01 -0500

    Fixed header selector. + Added info messages.

--- src/master/model/HTMLObjectWriter.java
+++ src/master/model/HTMLObjectWriter.java
@@ -4,4 +4,24 @@ package master.model;
  * Created by Thanh-Phong on 12/8/2015.
  */
 public class HTMLObjectWriter {
+
+    public void writeHTMLSection(HTMLSection section){
+
+    }
+
+    public void writeHTMLParagraph(HTMLParagraph paragraph){
+
+    }
+
+    public void writeHTMLList(HTMLList list){
+
+    }
+
+    public void writeHTMLHeader(HTMLHeader header){
+
+    }
+
+    public void writeHTMLFooter(String footer){
+
+    }
 }
